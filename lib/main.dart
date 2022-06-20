5c6148e4831466e6d1a30bf64e071126a0eaad61
intiu <ltpntu@gmail.com>
2022-06-20 19:31:29 +0700

    ltp

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:task_manager/modules/home/views/home_view.dart';
+import 'app/data/hive_data_store.dart';
+import 'app/shared/models/task.dart';
+
+Future<void> main() async {
+  await Hive.initFlutter();
+  // register adapter
+  Hive.registerAdapter<Task>(TaskAdapter());
+  // open boxes
+  var box = await Hive.openBox<Task>('tasks');
+  // delete data from previous day
+  box.values.forEach((task) {
+    if (task.createdAt.day != DateTime.now().day) {
+      box.delete(task.id);
+    }
+  });
+
+  runApp(BaseWidget(child: MyApp()));
+}
+
+class BaseWidget extends InheritedWidget {
+  BaseWidget({required this.child}) : super(child: child);
+  final HiveDataStore dataStore = HiveDataStore();
+  final Widget child;
+
+  static BaseWidget of(BuildContext context) {
+    final base = context.dependOnInheritedWidgetOfExactType<BaseWidget>();
+    if (base != null) {
+      return base;
+    } else {
+      throw StateError('Could not find ancestor widget of type BaseWidget');
+    }
+  }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return false;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quản lý công việc',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+              elevation: 0.0,
+              backgroundColor: Colors.white,
+              iconTheme: IconThemeData(color: Colors.black))),
+      home: const HomeView(),
+    );
+  }
+}
---
+++ lib/modules/home/views/home_view.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:task_manager/modules/home/widgets/task_widget.dart';
+import 'package:task_manager/app/shared/models/task.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:task_manager/main.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    final base = BaseWidget.of(context);
+    final box = base.dataStore.box;
+    return ValueListenableBuilder(
+        valueListenable: base.dataStore.listenToTasks(),
+        builder: (BuildContext context, Box<Task> box, Widget? child) {
+          var tasks = box.values.toList();
+          tasks.sort((a, b) => a.createdAt.compareTo(b.createdAt));
+          return Scaffold(
+              backgroundColor: Colors.white,
+              appBar: AppBar(
+                centerTitle: false,
+                title: GestureDetector(
+                    onTap: () {
+                      FocusScope.of(context).requestFocus(FocusNode());
+                    },
+                    child: Container(
+                        margin: EdgeInsets.only(left: 6),
+                        child: Text(
+                          'Bạn muốn làm gì hôm nay?',
+                          style: TextStyle(color: Colors.black),
+                        ))),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return Container(
+                                width: MediaQuery.of(context).size.width,
+                                padding: EdgeInsets.only(
+                                    bottom: MediaQuery.of(context)
+                                        .viewInsets
+                                        .bottom),
+                                margin: EdgeInsets.symmetric(horizontal: 16),
+                                child: ListTile(
+                                  title: TextField(
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: 'Nhập tên công việc'),
+                                    onSubmitted: (value) {
+                                      Navigator.pop(context);
+                                      var currentDate = DateTime.now();
+                                      DatePicker.showTimePicker(context,
+                                          showSecondsColumn: false,
+                                          showTitleActions: true,
+                                          onChanged: (date) {},
+                                          onConfirm: (date) {
+                                        if (value.isNotEmpty) {
+                                          var task = Task.create(
+                                              name: value, createdAt: date);
+                                          base.dataStore.addTask(task: task);
+                                        }
+                                      }, currentTime: DateTime.now());
+                                    },
+                                    autofocus: true,
+                                  ),
+                                ),
+                              );
+                            });
+                      },
+                      icon: Icon(Icons.add))
+                ],
+              ),
+              body: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  var task = tasks[index];
+                  return Dismissible(
+                      background: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.delete_outline,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text('Công việc này đã bị xóa',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ))
+                        ],
+                      ),
+                      onDismissed: (direction) {
+                        base.dataStore.deleteTask(task: task);
+                      },
+                      key: Key(task.id),
+                      child: TaskWidget(task: task));
+                },
+              ));
+        });
+  }
+}
